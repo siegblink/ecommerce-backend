8442888b0266d2e84290b583923a9a8787a5d7e8
Sieg <siegblink@gmail.com>
2020-03-25 04:45:58 +0800

    Refactor category route.

--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-
 const { create } = require('../controllers/category')
+const { findUserById } = require('../controllers/user')
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
 
-router.post('/category/create', create)
+router.param('userId', findUserById)
+router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create)
 
 module.exports = router
